51759006af63ced19cbf68c014db4739dba638f4
anthropomorphic <code@mikefdorst.com>
2014-01-08 13:18:31 -0800

    Implement LinkedList, fully tested Partially implement ListIterator, not tested Remove redundant 'list' package

---
+++ src/container/ordered/LinkedList.java
@@ -0,0 +1,161 @@
+package container.ordered;
+
+import java.lang.reflect.Array;
+import java.util.Collection;
+
+/**
+ * Michael Dorst
+ * CISP 430 Data Structures
+ * M Dixon
+ */
+
+public class LinkedList<E extends Comparable<E>> implements Collection<E> {
+  
+  class Node<E extends Comparable<E>> {
+    public E value;
+    public Node<E> next;
+    public Node<E> previous;
+    
+    Node(){}
+    Node(E value) {
+      this.value = value;
+    }
+  }
+  
+  /**
+   * Data declarations
+   */
+  private Node<E> head;
+  private Node<E> tail;
+  private int size;
+  
+  /**
+   * Collection
+   */
+  
+  @Override
+  public int size() {
+    return size;
+  }
+  
+  @Override
+  public boolean isEmpty() {
+    return size == 0;
+  }
+  
+  @Override
+  public boolean add(E e) {
+    if (isEmpty()) {
+      head = tail = new Node<>(e);
+    } else {
+      tail.next = new Node<>(e);
+      tail.next.previous = tail;
+      tail = tail.next;
+    }
+    size += 1;
+    return true;
+  }
+  
+  @Override
+  public void clear() {
+    head = tail = null;
+    size = 0;
+  }
+  
+  @Override
+  public ListIterator<E> iterator() {
+    return new ListIterator<>(head);
+  }
+  
+  @Override
+  public Object[] toArray() {
+    Object[] a = new Object[size];
+    int i = 0;
+    for(E e : this) {
+      a[i++] = e;
+    }
+    return a;
+  }
+  
+  @Override
+  public <T> T[] toArray(T[] a) {
+    if (a.length < size) {
+      @SuppressWarnings("unchecked")
+      T[] _a = (T[]) Array.newInstance(a.getClass().getComponentType(), size);
+      a = _a;
+    }
+    {
+      int i = 0;
+      for(E e : this) {
+        a[i++] = (T) e;
+      }
+    }
+    if (a.length > size) {
+      a[size] = null;
+    }
+    return a;
+  }
+  
+  @Override
+  public boolean remove(Object o) {
+    throw new UnsupportedOperationException();
+  }
+  
+  @Override
+  public boolean contains(Object o) {
+    throw new UnsupportedOperationException();
+  }
+  
+  @Override
+  public boolean containsAll(Collection<?> c) {
+    throw new UnsupportedOperationException();
+  }
+  
+  @Override
+  public boolean addAll(Collection<? extends E> c) {
+    throw new UnsupportedOperationException();
+  }
+  
+  @Override
+  public boolean removeAll(Collection<?> c) {
+    throw new UnsupportedOperationException();
+  }
+  
+  @Override
+  public boolean retainAll(Collection<?> c) {
+    throw new UnsupportedOperationException();
+  }
+  
+  /**
+   * Constructors
+   */
+  public LinkedList() {}
+  
+  public LinkedList(int nodes) {
+    this(nodes, null);
+  }
+  
+  public LinkedList(int size, E value) {
+    this.size = size;
+    head = new Node<>();
+    Node<E> n = head;
+    for (int i = 0; i < size; i++) {
+      n.next = new Node<>(value);
+      n = n.next;
+    }
+    tail = n.previous;
+    head = head.next;
+  }
+  
+  public LinkedList(Collection<? extends E> collection) {
+    size = collection.size();
+    head = new Node<>();
+    Node<E> n = head;
+    for(E e : collection) {
+      n.next = new Node<>(e);
+      n = n.next;
+    }
+    tail = n.previous;
+    head = head.next;
+  }
+}
---
+++ test/test/container/ordered/LinkedListTest.java
@@ -0,0 +1,153 @@
+package test.container.ordered;
+
+import container.ordered.LinkedList;
+import container.ordered.ListIterator;
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+/**
+ * Michael Dorst
+ * CISP 430 Data Structures
+ * M Dixon
+ */
+public class LinkedListTest {
+  
+  java.util.List<String> arrayList;
+  LinkedList<String> emptyList;
+  LinkedList<String> list;
+  
+  @Before
+  public void setUp() {
+    arrayList = new java.util.ArrayList<>();
+    for(int i = 0; i < 15; i++) {
+      arrayList.add("example string 2");
+    }
+    list = new LinkedList<>();
+    for(int i = 0; i < 100; i++) {
+      list.add(""+i);
+    }
+    emptyList = new LinkedList<>();
+  }
+  
+  @Test
+  public void add() {
+    assertThat(list.size(), is(100));
+    ListIterator<String> iter = list.iterator();
+    for(int i = 0; i < 100; i++) {
+      assertThat(iter.next(), is(""+i));
+    }
+    assertThat(iter.hasNext(), is(false));
+  }
+  
+  @Test
+  public void clear() {
+    assertThat(list.size(), is(100));
+    list.clear();
+    assertThat(list.size(), is(0));
+    for(String s : list) {
+      fail("list should be empty");
+    }
+    assertThat(list.iterator().hasNext(), is(false));
+    assertThat(list.iterator().hasPrevious(), is(false));
+  }
+  
+  @Test
+  public void foreach() {
+    int i = 0;
+    for(String s : list) {
+      assertThat(s, is(""+i));
+      i += 1;
+    }
+  }
+  
+  @Test
+  public void toArray_Raw() {
+    Object[] a = list.toArray();
+    assertThat(a.length, is(100));
+    int i = 0;
+    for(Object e : a) {
+      assertThat(e, is(instanceOf(String.class)));
+      assertThat((String) e, is(""+i++));
+    }
+  }
+  
+  @Test
+  public void toArray_Generic() {
+    String[] a = new String[100];
+    list.toArray(a);
+    {
+      int i = 0;
+      for(String s : a) {
+        assertThat(s, is(""+i++));
+      }
+    }
+    a = list.toArray(new String[99]);
+    {
+      int i = 0;
+      for(String s : a) {
+        assertThat(s, is(""+i++));
+      }
+    }
+    a = list.toArray(new String[101]);
+    {
+      for(int i = 0; i < 100; i++) {
+        assertThat(a[i], is(""+i));
+      }
+      assertThat(a[100], is(equalTo(null)));
+    }
+  }
+  
+  @Test
+  public void defaultConstructor() {
+    LinkedList<String> list = new LinkedList<>();
+    assertThat(list.isEmpty(), is(true));
+    assertThat(list.size(), is(0));
+    ListIterator<String> iter = list.iterator();
+    assertThat(iter.hasNext(), is(false));
+    assertThat(iter.hasPrevious(), is(false));
+  }
+  
+  @Test
+  public void sizeConstructor() {
+    LinkedList<String> list = new LinkedList<>(5);
+    assertThat(list.size(), is(5));
+    ListIterator<String> iter = list.iterator();
+    assertThat(iter.hasPrevious(), is(false));
+    for(int i = 0; i < 5; i++) {
+      assertThat(iter.hasNext(), is(true));
+      assertThat(iter.next(), is(equalTo(null)));
+      assertThat(iter.hasPrevious(), is(true));
+    }
+  }
+  
+  @Test
+  public void sizeAndValueConstructor() {
+    LinkedList<String> list = new LinkedList<>(10, "example string 1");
+    assertThat(list.size(), is(10));
+    ListIterator<String> iter = list.iterator();
+    assertThat(iter.hasPrevious(), is(false));
+    for(int i = 0; i < 10; i++) {
+      assertThat(iter.hasNext(), is(true));
+      assertThat(iter.next(), is(equalTo("example string 1")));
+      assertThat(iter.hasPrevious(), is(true));
+    }
+  }
+  
+  @Test
+  public void copyConstructor() {
+    LinkedList<String> list = new LinkedList<>(arrayList);
+    assertThat(list.size(), is(15));
+    ListIterator<String> iter = list.iterator();
+    assertThat(iter.hasPrevious(), is(false));
+    for(int i = 0; i < 15; i++) {
+      assertThat(iter.hasNext(), is(true));
+      assertThat(iter.next(), is(equalTo("example string 2")));
+      assertThat(iter.hasPrevious(), is(true));
+    }
+  }
+}
