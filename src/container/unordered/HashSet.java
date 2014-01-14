ca5dca3f0482c0a34f9273a41b664180fd20a8b4
anthropomorphic <code@mikefdorst.com>
2014-01-14 15:17:11 -0800

    Add (empty) HashSet class, with (empty) tests

---
+++ src/container/unordered/HashSet.java
@@ -0,0 +1,12 @@
+package container.unordered;
+
+/**
+ * Michael Dorst
+ * CISP 430 Data Structures
+ * M Dixon
+ *
+ * HashSet.java
+ */
+public class HashSet<E> {
+  
+}
---
+++ test/test/container/unordered/HashSetTest.java
@@ -0,0 +1,12 @@
+package test.container.unordered;
+
+/**
+ * Michael Dorst
+ * CISP 430 Data Structures
+ * M Dixon
+ *
+ * HashSetTest.java
+ */
+public class HashSetTest {
+
+}
